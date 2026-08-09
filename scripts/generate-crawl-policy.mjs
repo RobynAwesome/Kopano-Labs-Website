d4f5e8cc39b42aa21c89657cc36af4de9dacec94
Kholofelo Robyn Rababalela <rkholofelo@gmail.com>
2026-08-09 10:30:53 +0200

    fix: index verified Cars4Mars public artifacts in crawl policy

--- scripts/generate-crawl-policy.mjs
+++ scripts/generate-crawl-policy.mjs
@@ -5,11 +5,13 @@ const origin = manifest.site.origin.replace(/\/$/, '');
 const routes = [...manifest.routes]
   .filter((route) => route.index && route.crawl)
   .sort((a, b) => a.sitemapOrder - b.sitemapOrder);
+const indexArtifacts = manifest.indexArtifacts ?? [];
 
 const sitemap = [
   '<?xml version="1.0" encoding="UTF-8"?>',
   '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
   ...routes.map((route) => `  <url><loc>${origin}${route.path}</loc></url>`),
+  ...indexArtifacts.map((path) => `  <url><loc>${origin}${path}</loc></url>`),
   '</urlset>',
   '',
 ].join('\n');
@@ -30,4 +32,4 @@ const robots = [
 await writeFile(new URL('../public/sitemap.xml', import.meta.url), sitemap, 'utf8');
 await writeFile(new URL('../public/robots.txt', import.meta.url), robots, 'utf8');
 
-console.log(`Generated crawl policy for ${routes.length} public routes.`);
+console.log(`Generated crawl policy for ${routes.length} public routes and ${indexArtifacts.length} public artifacts.`);

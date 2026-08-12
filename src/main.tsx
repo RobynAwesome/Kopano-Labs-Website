d69ea38c51832f726fe4315aa4fc7842dc7e2159
Kholofelo Robyn Rababalela <rkholofelo@gmail.com>
2026-08-12 09:45:06 +0200

    mount governed site experience controls

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { SiteExperience } from './SiteExperience';
 import { startExperienceRuntime } from './experienceRuntime';
 import './styles.css';
 import './spatial.css';
@@ -8,10 +9,12 @@ import './directory.css';
 import './mission.css';
 import './now.css';
 import './foc.css';
+import './site-experience.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <SiteExperience />
   </React.StrictMode>,
 );
 
